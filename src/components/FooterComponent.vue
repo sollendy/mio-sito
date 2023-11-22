dae4c206a3c2bfaa9d95d2a1dd8b1806623f3286
sollendy <edlannino@tutanota.com>
2023-11-22 17:55:05 +0100

    creato componente e allestito futer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,16 @@
 <template>
     <footer class="bg-secondary">
-        <h1>foooooooooooooooooooooooooooooooooooooooooooooooooooter</h1>
+        <section class="saluti">
+            <div class="text-cont w-75 m-auto text-center text-light px-3 py-3">
+                <h3>Edoardo Lannino</h3>
+                <ul class="list-group">
+                    <li class="list-group-item bg-transparent text-light"><b>Telefono:</b> +39 340 843 5438</li>
+                    <li class="list-group-item bg-transparent text-light"><b>Mail:</b> edlannino@tutanota.com</li>
+                </ul>
+                <p class="d-md-none pt-2">Per maggiori informazioni sui miei profili e le mie piattaforme vi rimando alla sezione <a href="#">Contatti</a>.</p>
+                <h5 class="d-none d-md-block pt-2">Per maggiori informazioni sui miei profili e le mie piattaforme vi rimando alla sezione <a href="#">Contatti</a>.</h5>
+            </div>
+        </section>
     </footer>
 </template>
   
@@ -23,5 +33,18 @@ export default {
     }
 }
 </script>
-  
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+    footer {
+        .saluti {
+            background-color: rgba(49, 62, 80, 1);
+            /*.text-cont {
+                .list-group {
+                    li:hover {
+                        background-color: rgba(69, 85, 97, 1);
+                    }
+                }
+            }*/
+        }
+    }
+</style>
---
+++ src/views/ChiComponent.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'ChiComponent',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
