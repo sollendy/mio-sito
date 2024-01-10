8659f94081ea6ce66abe0af56c5eef90e912af37
sollendy <edlannino@tutanota.com>
2024-01-10 16:23:09 +0100

    problema padding momentaneamente risolto

--- src/views/ChiComponent.vue
+++ src/views/ChiComponent.vue
@@ -21,10 +21,12 @@ export default {
 <template>
     <section class="intro">
         <div class="sec-cont">
-            <div class="desc-foto">
-                <div class="img-cont text-center py-3 d-flex flex-column justify-content-center gap-2">
-                    <img src="https://picsum.photos/200/300" class="w-25 m-auto" alt="ah">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur voluptatum id aliquam at expedita odit aspernatur harum beatae, neque ad nostrum sunt quia non qui asperiores quae dolores. Blanditiis, aliquid!</p>
+            <div class="desc-foto text-center pt-4 pb-2 d-sm-flex flex-sm-column align-items-sm-center">
+                <div class="img-cont pb-2">
+                    <img src="https://picsum.photos/300/200" class="" alt="ah">
+                </div>
+                <div class="benv-text text-center m-auto w-75">
+                    <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur voluptatum id aliquam at expedita odit aspernatur harum beatae, neque ad nostrum sunt quia non qui asperiores quae dolores. Blanditiis, aliquid!</p>
                 </div>
             </div>
         </div>
